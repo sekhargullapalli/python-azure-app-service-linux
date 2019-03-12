4470baa2bcfb55357f6cc6f47a265fe01727025a
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-03-12 17:20:54 +0100

    sorted imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from appsettings import AppSettings
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from app import routes
+from appsettings import AppSettings
 
 app = Flask(__name__)
 app.config.from_object(AppSettings)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from app import routes
