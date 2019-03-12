6b3c622737889ca7fb2011e4a4d4f1f28a62c352
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-03-12 20:15:59 +0100

    added basic routes

--- app/approutes.py
+++ app/approutes.py
@@ -1,1 +1,22 @@
+import os
+from app import app 
 from app.appmodels import Countries
+from flask import render_template, jsonify, send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.png', mimetype='image/png')
+
+@app.route("/")
+def index():
+    countries = Countries.query.order_by(Countries.name)
+    return render_template('index.html',countries=countries)
+
+@app.errorhandler(404)
+def page_not_found_err(err):
+    return render_template('notfound.html')
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    return render_template('err.html',e=e)
