593367f3e21a7ad3c2a2147bbd96ec593490c0a0
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-03-12 17:30:18 +0100

    redone migration after adding models

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from app import approutes
 from appsettings import AppSettings
 
 app = Flask(__name__)
 app.config.from_object(AppSettings)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from app import approutes
